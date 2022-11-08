2ba3ce284d9eab9d13050b3fd51ff81a3abdb542
Hudson Oliveira <hudson.o.pereira@gmail.com>
2022-11-08 10:46:01 -0300

    set engine

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const prisma = new PrismaClient()
 const app = express()
 
 app.set("view engine", "ejs"); 
+app.set("views","./views")
 app.use(express.static(path.join("./"))); 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
